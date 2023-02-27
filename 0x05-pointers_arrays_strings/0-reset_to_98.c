3039e58d40d5f2f5a7cbc1696d4d33955a177e3a
mihret demisie <mihretdemisie2351@gmail.com>
2023-02-27 11:05:52 -0800

    ggsg

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,10 @@
 #include "main.h"
+
+/**
+ * reset_to_98 -> a function to update the reference of pointer
+ * @n: pointer to n to be updated
+  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
