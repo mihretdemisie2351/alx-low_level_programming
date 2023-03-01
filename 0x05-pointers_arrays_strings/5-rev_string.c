44b053c0700c5391a031f0cc529fc17e8528d279
mihret demisie <mihretdemisie2351@gmail.com>
2023-02-28 17:52:25 -0800

    627

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,8 +4,7 @@
 * rev_string -> reversing a string
 * @s: parameter s
 *
-*/                                                 
- void rev_string(char *s)
+*/                                                void rev_string(char *s)
 {
 char tmp;
 int i, length1, length2;
